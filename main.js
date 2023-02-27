c9a61159585f7ffb73263f78e6078e6117c1dc3d
Gabriel Carvalho <gabrielborges565@gmail.com>
2023-02-26 23:34:18 -0300

    First test with video

---
+++ main.js
@@ -0,0 +1,32 @@
+const APP_ID = "d63b4aa770a8401a813cf8e20a93e3af"
+const TOKEN = "007eJxTYPD6v2NPa533GRPWJwVGXfL8liELTv0VuqTSwPz9eLXNvzUKDClmxkkmiYnm5gaJFiYGhokWhsbJaRapRgaJlsapxolpHA9/JTcEMjLs4rzJysgAgSA+C0NuYmYeAwMAcSEgRw=="
+const CHANNEL = "main"
+
+const client = AgoraRTC.createClient({mode:'rtc', codec:'vp8'})
+
+let localTracks = []
+let remoteUsers = {}
+
+let joinAndDisplayLocalStream = async () => {
+
+    let UID = await client.join(APP_ID, CHANNEL, TOKEN, null)
+
+    localTracks = await AgoraRTC.createMicrophoneAndCameraTracks() 
+
+    let player = `<div class="video-container" id="user-container-${UID}">
+                        <div class="video-player" id="user-${UID}"></div>
+                  </div>`
+    document.getElementById('video-streams').insertAdjacentHTML('beforeend', player)
+
+    localTracks[1].play(`user-${UID}`)
+    
+    await client.publish([localTracks[0], localTracks[1]])
+}
+
+let joinStream = async () => {
+    await joinAndDisplayLocalStream()
+    document.getElementById('join-btn').style.display = 'none'
+    document.getElementById('stream-controls').style.display = 'flex'
+}
+
+document.getElementById('join-btn').addEventListener('click', joinStream)
