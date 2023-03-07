6d306d67006f415cd504a6e074783ee30897ffc7
Gabriel Carvalho <gabrielborges565@gmail.com>
2023-03-06 22:05:12 -0300

    Configurando o Django Channels

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,12 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "daphne",
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'chat',
+
+    'channels'
 ]
 
 MIDDLEWARE = [
@@ -123,3 +129,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ASGI_APPLICATION = "myproject.asgi.application"
